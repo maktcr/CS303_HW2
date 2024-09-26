e658c8e59a13ade6bb73b9d81d8de29336583693
Maktcr2022 <masonrob01@live.com>
2024-09-26 08:49:43 -0500

    Linked_List commit
    Final commit for the single linked list portion of the assignment

--- CS303_HW2.cpp
+++ CS303_HW2.cpp
@@ -1,22 +1,25 @@
-// CS303_HW2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
 #include "Single_Linked_List.h"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Single_Linked_List test
+    Single_Linked_List<int> list;
+    string input;
+    list.push_back(10);
+    list.push_back(11);
+    list.push_back(5);
+    cout << list.pop_back() << " was removed from the end" << endl;
+    cout << list.pop_front() << " was removed from the front" << endl;
+    cout << "Enter a number to locate: ";
+    cin >> input;
+    cout << "Number was found at index " << list.find(stoi(input));
+
+
+
+    //Q2::Write a C++ program to implement a stack of integers using a vector with push and pop operations.
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
--- Single_Linked_List.h
+++ Single_Linked_List.h
@@ -1,7 +1,16 @@
 #pragma once
+#ifndef SINGLE_LINKED_LIST_H
+#define SINGLE_LINKED_LIST_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstddef>
+using namespace std;
 
 template<typename Item_Type>
 class Node {
+public:
 	Item_Type data;
 	Node* next;
 
@@ -35,3 +44,231 @@ public:
 	size_t find(const Item_Type& item) const;
 
 };
+
+
+//Constructor
+template<typename Item_Type>
+Single_Linked_List<Item_Type>::Single_Linked_List()
+	: head(nullptr), tail(nullptr), num_items(0) {}
+
+//Destructor
+template<typename Item_Type>
+Single_Linked_List<Item_Type>::~Single_Linked_List() {
+	while (!empty()) {
+		pop_front();
+	}
+}
+
+template<typename Item_Type>
+void Single_Linked_List<Item_Type>::push_front(const Item_Type& item) {
+
+	Node* new_head = new Node(item);
+
+	//If the linked list is empty, head and tail is set to new_head
+	//else, set new_head next to the current head 
+	if (empty()) {
+		tail = new_head;
+	}
+	else {
+		new_head->next = head;
+	}
+	//Now that new_head next is pointing to the old head,
+	//set the head to new_head and incriment num_items.
+	head = new_head;
+	num_items++;
+}
+
+template<typename Item_Type>
+void Single_Linked_List<Item_Type>::push_back(const Item_Type& item) {
+
+	Node<Item_Type>* new_tail = new Node<Item_Type>(item);
+
+	//If the linked list is empty, head and tail is set to new_tail
+	//else, set new_tail next to the current head 
+	if (empty()) {
+		head = new_tail;
+	}
+	else {
+		tail->next = new_tail;
+	}
+	//Now that tail->next is set to the new tail (linking it to the list),
+	//set the tail to new_tail and incriment num_items.
+	tail = new_tail;
+	num_items++;
+}
+
+template<typename Item_Type>
+Item_Type Single_Linked_List<Item_Type>::pop_front() {
+
+	if (empty()) {
+		//cannot pop from empty list
+	}
+
+	//Save head data to return it after its deleted.
+	Item_Type item = head->data;
+
+	//Node to be deleted.
+	Node<Item_Type>* temp = head;
+
+	//Set new head to the second item in the list, then delete the temp node.
+	head = head->next;
+	delete temp;
+
+	//If list is now empty (head == nullptr), update the tail to nullptr.
+	if (head == nullptr) {
+		tail = nullptr;
+	}
+
+	//Decrement num_items and return the popped item.
+	num_items--;
+	return item;
+}
+
+template<typename Item_Type>
+Item_Type Single_Linked_List<Item_Type>::pop_back() {
+
+	if (empty()) {
+		cout << "List is empty" << endl;
+	}
+
+	//Save tail data to return it aftet its deleted.
+	Item_Type item = tail->data;
+
+	//If the head is the tail (there is only one entity in the list),
+	//delete the head, and set both head and tail to nullptr.
+	if (head == tail) {
+		delete head;
+		head = nullptr;
+		tail = nullptr;
+	}
+	else {
+
+		//If the list has more than one entity, we need to find the node before the tail.
+		//Since this is a single linked list, we have to iterate through the list in order to 
+		//find the node before the tail.
+		Node<Item_Type>* temp = head;
+		//Iterate through List until temp->next is equal to tail.
+		while (temp->next != tail) {
+			temp = temp->next;
+		}
+
+		//Now that temp is the node before tail, delete the tail and set temp to the new tail,
+		//Then set tail->next to nullptr.
+		delete tail;
+		tail = temp;
+		tail->next = nullptr;
+	}
+
+	//Decriment num_items and return the popped item.
+	num_items--;
+	return item;
+}
+
+template<typename Item_Type>
+Item_Type Single_Linked_List<Item_Type>::front() const {
+	if (empty()) {
+		cout << "List is empty" << endl;
+	}
+	return head->data;
+}
+
+template<typename Item_Type>
+Item_Type Single_Linked_List<Item_Type>::back() const {
+	if (empty()) {
+		cout << "List is empty" << endl;
+	}
+	return tail->data;
+}
+
+template<typename Item_Type>
+bool Single_Linked_List<Item_Type>::empty() const {
+
+	//If the list is empty, return true. If it isnt empty, return false.
+	if (num_items == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template<typename Item_Type>
+void Single_Linked_List<Item_Type>::insert(size_t index, const Item_Type& item) {
+
+	//If the given index is 0 or less, use the push_front function to add the item to the front.
+	if (index <= 0) {
+		push_front(item);
+	}
+	//If the given index is greater than or equal to the size of the list, use the push_back function
+	//to add it to the end.
+	else if (index >= num_items) {
+		push_back(item);
+	}
+	//If the given index falls within the scope of the list, we must iterate through the list until we reach the node
+	//before the given index.
+	else {
+		Node* new_node = new Node(item);	//item to be added
+		Node* temp = head;
+		for (size_t i = 0; i < index - 1; i++) {
+			temp = temp->next;
+		}
+
+		//Now that temp is the node one index before the position we want to add a node,
+		//link the temp node to the list by setting the new_node next to temp->next,
+		//and then updated temp->next to reflect the change.
+		new_node->next = temp->next;
+		temp->next = new_node;
+		num_items++;	//incriment num_items
+	}
+}
+
+template<typename Item_Type>
+bool Single_Linked_List<Item_Type>::remove(size_t index) {
+
+	//If given index is beyond the end of the list, return false.
+	if (index < 0 || index >= num_items) {
+		return false;
+	}
+	//If given index is at the front, use the pop_front function.
+	if (index == 0) {
+		pop_front();
+	}
+	else {
+		//Iterate through the list until we reach the index before the one we want to remove.
+		Node* temp = head;
+		for (size_t i = 0; i < index - 1; i++) {
+			temp = temp->next;
+		}
+		//Set node to_remove, if this node is the tail, set temp to be the new tail.
+		Node* to_remove = temp->next;
+		if (to_remove == tail) {
+			tail = temp;
+		}
+		//Set temp->next to the next pointer of the item we are deleting, then delete the node and decriment num_items.
+		temp->next = to_remove->next;
+		delete to_remove;
+		num_items--;
+	}
+	//Function was successful, return true.
+	return true;
+}
+
+template<typename Item_Type>
+size_t Single_Linked_List<Item_Type>::find(const Item_Type& item) const {
+
+	//Use temp node to iterate through the list until item is found
+	Node<Item_Type>* temp = head;
+	size_t index = 0;	//Start at index 0
+	while (temp != nullptr) {	//While temp (iterator) is not past the end of the list
+		if (temp->data == item) {
+			return index;	//Return position of the item when it is found
+		}
+		//Iterate
+		temp = temp->next;
+		index++;
+	}
+	return num_items; // Return size if not found
+}
+
+
+#endif
